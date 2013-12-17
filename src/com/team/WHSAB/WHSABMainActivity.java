92dfbba8e5725c7ca3bbd6103bef2297dfa2c7e4
Zachary Buffone <zacharybuffone@gmail.com>
2013-12-16 21:49:35 -0500

    Added doge messages to main_activity Added doge ImageView to main_activity Linked ImageView to resource in WHSABMainActivity Doge is love stuff too

--- src/com/team/WHSAB/WHSABMainActivity.java
+++ src/com/team/WHSAB/WHSABMainActivity.java
@@ -1,8 +1,8 @@
 package com.team.WHSAB;
-
 import com.example.lolol.R;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.app.Activity;
 
 public class WHSABMainActivity extends Activity {
@@ -12,8 +12,16 @@ public class WHSABMainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
+		ImageView dogeImageView = (ImageView) findViewById(R.id.dogeImageView);
+		dogeImageView.setImageResource(R.drawable.doge);
+		
+		//Doge is life, doge is love.
+		//Let us never forget, thy who bring warmth
+		//upon our shallow, cold hearts. The doge of truth. The doge
+		//of compassion. Bless.
+		//							- Dogus 69:420
 		
 	}
-
+	
 
 }
