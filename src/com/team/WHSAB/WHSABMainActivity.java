40036f32b1d78a1a15be3ff8cb50190ff6223fac
Zachary Buffone <zacharybuffone@gmail.com>
2013-12-16 21:06:11 -0500

    Renamed package com.example.lolol (lol) to com.team.WHSAB Renamed class MainActivity to WHSABMainActivity

---
+++ src/com/team/WHSAB/WHSABMainActivity.java
@@ -0,0 +1,19 @@
+package com.team.WHSAB;
+
+import com.example.lolol.R;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class WHSABMainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		
+	}
+
+
+}
